974caf52ae917691e56d0a7f7f0615c39ceeac85
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-12 01:55:22 -0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/padavanonly/immortalwrt
+git clone --depth 1 https://github.com/padavanonly/immortalwrt && mv immortalwrt/* . ;\ rm -rf immortalwrt
 
 exit 0
